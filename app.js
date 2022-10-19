fc2ec44237c2f390fcdc1ac2e7de9634b74c91ec
jose_pro <sejoma989@gmail.com>
2022-10-19 12:39:06 -0500

    Despliegue app 2022-10-19

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 // handlebars
@@ -15,36 +16,35 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
 // Servir contenido estatico
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 
 // ruta principal + controlador manejador de la vista principal
-app.get("/", (req, res) => {
-    res.render('home', {
-        nombre:'Fernando Herrera',
-        titulo:'Curso de Node'
-    });
-});
+// app.get("/", (req, res) => {
+//     res.render('home', {
+//         nombre:'Fernando Herrera',
+//         titulo:'Curso de Node'
+//     });
+// });
 
-app.get("/generic", (req, res) => {
-    res.render('generic', {
-        nombre:'Fernando Herrera',
-        titulo:'Curso de Node'
-    });
-});
+// app.get("/generic", (req, res) => {
+//     res.render('generic', {
+//         nombre:'Fernando Herrera',
+//         titulo:'Curso de Node'
+//     });
+// });
 
-app.get("/elements", (req, res) => {
-    res.render('elements', {
-        nombre:'Fernando Herrera',
-        titulo:'Curso de Node'
-    });
-});
+// app.get("/elements", (req, res) => {
+//     res.render('elements', {
+//         nombre:'Fernando Herrera',
+//         titulo:'Curso de Node'
+//     });
+// });
 
-// ruta por defecto en caso que no encuentre la ruta
 app.get("*", (req, res) => {
-    res.sendFile( __dirname + '/public/404.html');
+    res.sendFile( __dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
